e759d16aca38b1226669888c5ad701db050a04ce
Nam <vinhnam311003@gmail.com>
2025-03-25 10:32:15 +0700

    add App.java and AppTest.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package Lab_2;
+
+public class App {
+	public boolean isEvenNumber(int input) {
+		if(input % 2 == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+}
---
+++ AppTest.java
@@ -0,0 +1,28 @@
+package Lab_2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+	
+	
+	@Test
+	public void testIsEvenNumber2() {
+		App demo1 = new App();
+		boolean result = demo1.isEvenNumber(2);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testIsEvenNumber4() {
+		App demo1 = new App();
+		boolean result = demo1.isEvenNumber(4);
+		assertTrue(result);
+	}
+}
